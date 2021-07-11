f82280be3282d810b326354b0f524709f9f1bf33
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-11 10:15:33 +0200

    New events examples

---
+++ eventexamples.js
@@ -0,0 +1,19 @@
+const events = require("events");
+var eventEmitter = new events.EventEmitter();
+
+var connectHandler = function connected(){
+    console.log("inside-1");
+    eventEmitter.emit("data_sent");
+}
+
+var connect2handler = function twoConnected(){
+    console.log("inside-2");
+    eventEmitter.emit("new_data");
+}
+
+eventEmitter.on("just_data", connectHandler);
+eventEmitter.on("data_sent", function(){
+    console.log("data received...");
+})
+eventEmitter.emit("just_data");
+
