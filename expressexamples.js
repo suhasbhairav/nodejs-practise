ccedcf229dfb8da527e2b846ca25cb4b8ea678fc
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-12 19:20:25 +0200

    Update express

--- expressexamples.js
+++ expressexamples.js
@@ -8,7 +8,7 @@ app.get("/", function (req, res){
 });
 
 app.get("/numbers", function (req, res){
-    var arr = new Array(3);
+    var arr = new Array();
     arr.push(1);
     arr.push(2);
     arr.push(3);
@@ -16,8 +16,17 @@ app.get("/numbers", function (req, res){
 });
 
 app.get("/names", function (req, res){
-    var arr = new Array(3);
+    var arr = new Array();
     arr.push("Google");
     arr.push("Facebook");
     arr.push("Microsoft");
+
+    res.send(arr);
 });
+
+var server = app.listen(8081, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Listening to %s:%s", host, port);
+})
