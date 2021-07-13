353c3b191145f618a8645cb1c80cfb401c2acc49
Suhas Bhairav <suhasbhairav@gmail.com>
2021-07-13 22:06:25 +0200

    File Reader

--- filereader.js
+++ filereader.js
@@ -5,3 +5,11 @@ console.log(data.toString());
 const fileReaderHandler = require("fs");
 var content = fileReaderHandler.readFileSync("input.txt");
 console.log(content.toString())
+
+const fs1 = require("fs");
+var dataContent = fs1.readFileSync("test.txt");
+console.log(dataContent.toString());
+
+const fs2 = require("fs");
+var content2 = fs2.readFileSync("input.txt");
+console.log(content2.toString());
